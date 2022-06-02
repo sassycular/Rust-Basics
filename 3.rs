62c3ef77db65905b0dababb17316a5d61f97342e
JD <57875970+sassycular@users.noreply.github.com>
2022-06-02 18:40:24 +0530

    Update 3.rs

--- 3.rs
+++ 3.rs
@@ -1,7 +1,9 @@
 // TODO print array elements in reverse order
 
-func main() {
-    let mut arr:[i32;10] = [0,1,2,3,4,5,6,7,8,9];
-    
+fn main() {
+  let mut arr:[i32;10] = [0,1,2,3,4,5,6,7,8,9];
+  println!("The array before reversing: {:?}", arr);
+  arr.reverse();
+  println!("The array after reversing: {:?}", arr);
 }
 
