5be67574d4d480058eff89490357463ee42552d8
JD <57875970+sassycular@users.noreply.github.com>
2022-06-02 16:42:56 +0530

    Create 5.rs
    TODO - Assign a value to a variable
    TODO - Use References to print name as many times as we do

---
+++ 5.rs
@@ -0,0 +1,11 @@
+fn print_name(name: String) {
+    println!("This is {}", name);
+}
+
+fn main() {
+    // TODO - Assign a value to a variable
+    let name = String::from("");
+    // TODO - Use References to print name as many times as we do
+    print_country(); // Call a function with a variable as an argument
+    print_country(); // Call a function with a variable as an argument again
+}
