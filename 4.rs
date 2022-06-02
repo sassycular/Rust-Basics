ff79c7cb4a45339ae8c07b8f916a8475a97dd5e0
JD <57875970+sassycular@users.noreply.github.com>
2022-06-02 16:40:45 +0530

    Create 4.rs
    TODO Match the sky and temperature and print the appropriate message

---
+++ 4.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let sky = "cloudy"; // can be "cloudy", "sunny", "rainy"
+    let temperature = "warm"; // can be "warm", "cold", "freezing"
+
+     // TODO Match the sky and temperature and print the appropriate message
+     /*
+        cloudly + warm = It's cloudly but it's warm
+        cloudly + cold = Plesant weather
+        cloudly + freezing =It's freezing outside
+        sunny + warm = It's hot sunny day
+        sunny + cold = It's a cold sunny day
+        rainy + warm = It's raining
+        rainy + cold = It's raining and it's cold
+        rainy + freezing = It's raining and it's freezing
+        others = Not sure what to do
+     */
+
+    match () {
+    }
+}
