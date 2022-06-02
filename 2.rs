a75bf23833132dea513c3f51bd80faa8e70739be
JD <57875970+sassycular@users.noreply.github.com>
2022-06-02 18:25:44 +0530

    Update 2.rs

--- 2.rs
+++ 2.rs
@@ -1,1 +1,13 @@
 // TODO Read two numbers from Command Line Arguments and print the sum of the numbers 
+// install the crate text_io - needed for this code 
+
+#[macro_use] extern crate text_io;
+fn main(){
+  println!("Enter the first number: "); 
+  let p: i32 = read!();
+  println!("Enter the second number: ");
+  let q: i32 = read!();
+  let sum = p + q;
+  println!("The sum of the numbers is: {}", sum);
+}
+
