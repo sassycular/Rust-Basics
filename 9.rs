4b31681202c65712a7906188c452f3414a8f645b
JD <57875970+sassycular@users.noreply.github.com>
2022-06-02 16:52:46 +0530

    Create 9.rs
    TODO Number gussing game with rust

---
+++ 9.rs
@@ -0,0 +1,10 @@
+// TODO Number gussing game with rust
+
+// Reference https://www.geeksforgeeks.org/number-guessing-game-in-python/
+
+
+fn main() {
+    let n1: u8; // TODO Generate a random number between 1 and 100
+    println!("Guess the number!");
+    // TODO: Build a loop that will continue until the user guesses the numbers
+}
