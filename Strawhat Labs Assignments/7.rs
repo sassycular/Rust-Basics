9848561b7342933ea10564e5ea5423e97dd0641a
JD <57875970+sassycular@users.noreply.github.com>
2022-06-04 18:51:36 +0530

    Update 7.rs

--- Strawhat Labs Assignments/7.rs
+++ Strawhat Labs Assignments/7.rs
@@ -1,1 +1,10 @@
+//TODO: Send mutable reference to function and print string
+fn main() {
+    let mut s = String::from("Hello");
+    change(&mut s);
+    println!("{}", s);
+}
 
+fn change(some_string: &mut String) {
+    some_string.push_str(", Solana");
+}
