08e4a048ab4b3215cc9171d728c15dd5ea288a36
JD <57875970+sassycular@users.noreply.github.com>
2022-06-04 17:56:45 +0530

    Update 5.rs

--- Strawhat Labs Assignments/5.rs
+++ Strawhat Labs Assignments/5.rs
@@ -1,11 +1,11 @@
 fn print_name(name: String) {
-    println!("This is {}", name);
+  println!("This is {}", name);
 }
 
 fn main() {
     // TODO - Assign a value to a variable
-    let name = String::from("");
+    let name = String::from("Jinx");
     // TODO - Use References to print name as many times as we do
-    print_country(); // Call a function with a variable as an argument
-    print_country(); // Call a function with a variable as an argument again
+    print_name(name.clone()); // Call a function with a variable as an argument
+    print_name(name.clone()); // Call a function with a variable as an argument again
 }
