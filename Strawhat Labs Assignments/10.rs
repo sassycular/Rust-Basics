154d9493c28c973d1cbaa169c7eeef4277469c1b
JD <57875970+sassycular@users.noreply.github.com>
2022-06-04 20:12:49 +0530

    Update 10.rs

--- Strawhat Labs Assignments/10.rs
+++ Strawhat Labs Assignments/10.rs
@@ -1,1 +1,14 @@
-// TODO: Get input from user and append it to a file called input.txt
+// TODO: Get input from user and append it to a file called input txt
+use std::io::prelude::*;
+use text_io::read;
+use std::fs::OpenOptions;
+
+fn main() {
+  let a:String = read!();
+  let mut file = OpenOptions::new()
+            .write(true)
+            .append(true)
+            .open("src/file.txt")
+            .unwrap();
+  writeln!(file, "{}", a);
+}
