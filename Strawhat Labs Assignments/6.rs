df8b65e7e7325341764e1170d00630cf6581db10
JD <57875970+sassycular@users.noreply.github.com>
2022-06-04 18:34:58 +0530

    Update 6.rs

--- Strawhat Labs Assignments/6.rs
+++ Strawhat Labs Assignments/6.rs
@@ -1,3 +1,27 @@
 // ToDo - Create a strut Shape with two variables length and width and implement Area and Perimeter methods
 // Area = length * width 
 // Perimeter = 2 * (length + width)
+
+struct Groot {
+  length: i32,
+  width: i32
+}
+
+impl Groot {
+  fn Area(&self)  -> i32 {
+    self.length * self.width
+  }
+  fn Peri(&self) -> i32 {
+    2 * (self.length + self.width)
+  }
+}
+
+fn main() {
+  let PixieDust = Groot{
+    length: 5,
+    width: 7
+  };
+  println!("The area of Groot is: {:?}", PixieDust.Area());
+  println!("The perimeter of Groot is: {:?}", PixieDust.Peri());
+  
+}
