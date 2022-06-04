801d83aadf9fbdf42bd769768330bb93859a8c8f
JD <57875970+sassycular@users.noreply.github.com>
2022-06-04 19:24:56 +0530

    Update 8.rs

--- Strawhat Labs Assignments/8.rs
+++ Strawhat Labs Assignments/8.rs
@@ -1,1 +1,29 @@
 // ToDO throw a custom error if the user enters a negative number
+#[macro_use] extern crate text_io;
+
+struct num {
+  x: i32
+}
+
+pub trait Number {
+  fn ntype(&self);
+}
+
+impl Number for num {
+    fn ntype(&self) {
+    if self.x < 0 {
+      println!("You have entered a negative number");
+    }
+  }
+}
+
+fn main() {
+  println!("Enter an integer: ");
+  let y = num {
+    x: read!()
+  };
+
+  y.ntype();
+  
+  
+}
